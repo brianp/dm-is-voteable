d59bc7f1cfae13ce071d2bc65f6e98e5d73d694d
brianp <brian.o.pearce@gmail.com>
2013-09-14 03:03:56 -0700

    Test for error messages.

--- spec/voteable/vote_spec.rb
+++ spec/voteable/vote_spec.rb
@@ -18,4 +18,16 @@ describe Voteable::Vote do
     @model.vote('Brian').saved?.must_equal true
     @model.vote('Brian').saved?.must_equal false
   end
+
+  it 'errors include time_between_votes' do
+    subject.saved?.must_equal true
+    @model.vote('Brian')
+    @model.vote('Brian').errors.keys.must_include :time_between_votes
+  end
+
+  it 'errors with message' do
+    subject.saved?.must_equal true
+    @model.vote('Brian')
+    @model.vote('Brian').errors.values.must_include ['You have already voted']
+  end
 end
