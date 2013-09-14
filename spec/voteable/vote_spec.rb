4e2c03db4dc452d4d3b52328726bc3c480547b68
brianp <brian.o.pearce@gmail.com>
2013-09-14 01:37:34 -0700

    Better spacing and proc/validation test.

--- spec/voteable/vote_spec.rb
+++ spec/voteable/vote_spec.rb
@@ -2,15 +2,20 @@ require 'spec_helper'
 
 describe Voteable::Vote do
 
- subject { @model.vote }
+  subject { @model.vote }
 
- before do
-   Voteable::Vote.destroy
-   @model = TestModel.create
- end
+  before do
+    Voteable::Vote.destroy
+    @model = TestModel.create
+  end
 
- it 'returns voteable' do
-   subject.voteable.id.must_equal @model.id
- end
+  it 'returns voteable' do
+    subject.voteable.must_equal @model
+  end
 
+  it 'validates with a proc' do
+    subject.saved?.must_equal true
+    @model.vote('Brian').saved?.must_equal true
+    @model.vote('Brian').saved?.must_equal false
+  end
 end
